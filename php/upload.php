e24fb88ece391e32bf31c17c6745521f8fb2ff86
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-02-24 22:47:31 +0400

    POST upload.php 405 (Method Not Allowed)

--- php/upload.php
+++ php/upload.php
@@ -1,6 +1,6 @@
 <?php
   $file_name =  $_FILES['file']['name'];
   $tmp_name = $_FILES['file']['tmp_name'];
-  $file_up_name = time().$file_name;//makingf file name dynamic by adding time before file name
-  move_uploaded_file($tmp_name, "files/".$file_up_name);//moving file to the specific folder with dynamic name
+  $file_up_name = time().$file_name;
+  move_uploaded_file($tmp_name, "files/".$file_up_name);
 ?>
